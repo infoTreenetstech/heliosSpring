6579355c79cebbd96570438bb74fc0d75578b0f5
Sunny <gogawalesunny58@gmail.com>
2023-10-27 11:07:14 +0530

    first commit

---
+++ src/test/java/com/treenets/finance/project/FinanceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treenets.finance.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
